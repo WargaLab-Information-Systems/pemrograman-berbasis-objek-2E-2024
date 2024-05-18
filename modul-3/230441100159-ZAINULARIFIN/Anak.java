8415b36cfa35dc95c45d6edbce30b1e66cc97969
uiqwdiuuqi <emailgitzainularifin4216@gmail.com>
2024-05-18 10:46:22 +0700

    230441100159-ZAINULARIFIN-DEVIDWINOVITASARI-MODUL3

---
+++ modul-3/230441100159-ZAINULARIFIN/Anak.java
@@ -0,0 +1,9 @@
+
+package perpustakaan;
+
+public class Anak extends Buku {
+    public Anak (String penulis, String publisher, String kategori, int stok, String tahunT) {
+        super(penulis,publisher,"A",kategori,stok,tahunT );
+    }
+
+}
